75db180cbfaa8cda56fca77f074863a0c0c6c33d
Raghav Vashisht <raghav2011rv@gmail.com>
2019-10-22 01:08:12 +0530

    Added data models and fixed bugs

--- app/app.py
+++ app/app.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from passlib.hash import sha256_crypt
-# import User
 
 app = Flask(__name__)
 
@@ -13,10 +11,12 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    enrolment = db.Column(db.String(20), unique=True, nullable=False, primary_key=True)
+    __tablename__ = 'user'
+    enrolment = db.Column(db.String(20), nullable=False, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    batch = db.Column(db.String(10), nullable=False)
+    batch = db.Column(db.String(10))
+    timetable = db.relationship("TimeTable", backref="User")
 
     def __init__(self, enrolment, name, password, batch):
         self.enrolment = enrolment
@@ -24,15 +24,43 @@ class User(db.Model):
         self.password = password
         self.batch = batch
 
+class TimeTable(db.Model):
+    __tablename__ = 'timetable'
+    day = db.Column(db.String(10), nullable = False)
+    batch = db.Column(db.String(10), primary_key=True)
+    teacherName = db.Column(db.String(255), nullable = False)
+    hallName = db.Column(db.String(40), nullable = False)
+    time = db.Column(db.String(25), nullable = False)
+    subject = db.Column(db.String(30), nullable = False)
+    enrolment = db.Column(db.String(20), db.ForeignKey('user.enrolment'))
+    hall = db.relationship("LectureHall", backref="TimeTable")
 
-@app.route('/add', methods=['POST'])
-def demo():
-    json = request.json
-    password = sha256_crypt.hash(json['password'])
-    user = User(json['enrolment'],json['name'],password,json['batch'])
-    db.session.add(user)
-    db.session.commit()
-    return jsonify("Success")
+    def __init__(self,day,batch,teacherName,hallName,time,subject):
+        self.day = day
+        self,batch = batch
+        self.teacherName = teacherName
+        self.hallName = hallName
+        self.time = time
+        self.subject = subject
+
+class LectureHall(db.Model):
+    __tablename__ = 'lecturehall'
+    name = db.Column(db.String(40), primary_key = True)
+    macAddress = db.Column(db.Text, nullable = False)
+    batch = db.Column(db.String(10), db.ForeignKey('timetable.batch'))
+
+    def __init__(self,name,address):
+        self.name = name
+        self.macAddress = address
+
+
+# @app.route('/add', methods=['POST'])
+# def demo():
+#     json = request.json
+#     user = User(json['enrolment'],json['name'],json['password'],json['batch'])
+#     db.session.add(user)
+#     db.session.commit()
+#     return jsonify({"Success"})
 
 if __name__ == '__main__':
     app.run(debug=True)
